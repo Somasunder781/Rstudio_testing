7ae48ee00b1cf95a9f4ee929d623ffdcea98f6ff
Somasunder781 <1932049mds@cit.edu.in>
2020-07-26 12:21:37 +0530

    3rd
    3rd

--- Printing.R
+++ Printing.R
@@ -2,6 +2,4 @@ print("this file is created within R stdio")
 
 print("Now its available on git hub")
 
-Print("Its second time , i am writing ")
-c=2*3
-print(c)
+Print("Its 3rd time , i am writing ")
